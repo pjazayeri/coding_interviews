85371bc89c475c24cbfc3f206cc2b6389f49ce19
Paul (Neema) Jazayeri <pauljazayeri@gmail.com>
2018-10-27 13:51:47 -0700

    Initial commit

---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] {2, 7, 11, 15 };
+            var target = 9;
+            var result = Solution.TwoSum(nums, target);
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ LeetCode/LeetCode/TwoSum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public static class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> nums_checked = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+                if (nums_checked.ContainsKey(target - nums[i]))
+                    return new int[] { nums_checked[target - nums[i]], i };
+                else if (!nums_checked.ContainsKey(nums[i])) nums_checked.Add(nums[i], i);
+            return new int[] { };
+        } 
+    }
+}
+
